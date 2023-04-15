860c33c80c81d2fd04e598d253721c63b91846d4
Yuan <zy21@uakron.edu>
2023-04-15 08:52:09 -0400

    Update tutor list style

--- components/TutorList.js
+++ components/TutorList.js
@@ -16,61 +16,54 @@ export default function TutorList() {
         fetchTutors();
       }, []);
 
-    return (
-        <div className="overflow-x-auto bg-gray-100">
-            {tutors.length > 0 ? (
-            <table className="min-w-full divide-y-2 divide-gray-200 text-sm text-justify">
-                <thead>
-                <tr>
-                    <th
-                    className="whitespace-nowrap px-4 py-2 font-medium text-gray-900"
-                    >
-                    Tutor
-                    </th>
-                    <th
-                    className="whitespace-nowrap px-4 py-2 font-medium text-gray-900"
-                    >
-                    Major
-                    </th>
-                    <th
-                    className="whitespace-nowrap px-4 py-2 font-medium text-gray-900"
-                    >
-                    Course
-                    </th>
-                    <th className="px-4 py-2"></th>
-                </tr>
-                </thead>
-
-                <tbody className="divide-y divide-gray-200">
-
-                    {tutors.map((tutor) => (
-                        <tr key={tutor.sid}>
-                            <td className="whitespace-nowrap px-4 py-2">
-                                <p className='text-gray-700'>{tutor.student.fname} {tutor.student.lname}</p>
-                                <p className='text-gray-400 text-xs'>{tutor.student.email}</p>
-                            </td>
-                            <td className="whitespace-nowrap px-4 py-2 text-gray-700">
-                                {tutor.major.map((major) => (
-                                    <p>{major}</p>
-                                ))}
-                            </td>
-                            <td className="whitespace-nowrap px-4 py-2 text-gray-700">
-                                {tutor.courses.map((course) => (
-                                    <p>{course}</p>
-                                ))}
-                            </td>
-                            <Link href={`/tutor/${tutor.sid}`}>
-                                <button className="mx-2 my-2 bg-white transition duration-150 ease-in-out hover:border-gray-700 hover:text-gray-600 rounded border border-gray-600 text-gray-500 px-6 py-2 text-xs">
-                                    View
-                                </button>
-                            </Link>
+      return (
+        <div className="w-full sm:px-6">
+            <div className="bg-white shadow px-4 md:px-10 pt-4 md:pt-7 pb-5 overflow-y-auto">
+                <table className="w-full whitespace-nowrap">
+                    <thead>
+                        <tr className="h-16 w-full text-sm leading-none text-gray-800">
+                            <th className="font-normal text-left pl-4">Tutor</th>
+                            <th className="font-normal text-left pl-12">Major</th>
+                            <th className="font-normal text-left pl-12">Courses</th>
+                            <th className="font-normal text-left pl-20">Status</th>
                         </tr>
-                    ))}
-                </tbody>
-            </table>
-            ) : (
-                <p>Loading tutors...</p>
-            )}
+                    </thead>
+                    <tbody className="w-full">
+                        {tutors.map((tutor) => (
+                            <tr key={tutor.sid} className="h-20 text-sm leading-none text-gray-800 bg-white hover:bg-gray-100 border-b border-t border-gray-100">
+                                <td className="pl-4 cursor-pointer">
+                                    <div className="flex items-center">
+                                        <div className="pl-4">
+                                            <p className="font-medium">{tutor.student.fname} {tutor.student.lname}</p>
+                                            <p className="text-xs leading-3 text-gray-600 pt-2">{tutor.student.email}</p>
+                                        </div>
+                                    </div>
+                                </td>
+                                <td className="pl-12">
+                                    {tutor.major.map((major) => (
+                                        <p className="text-sm font-medium leading-none text-gray-800">{major}</p>
+                                    ))}
+                                </td>
+                                <td className="pl-12">
+                                    {tutor.courses.map((course) => (
+                                        <p className="text-xs leading-3 text-gray-600 mt-2">{course}</p>
+                                    ))}
+                                </td>
+                                <td className="pl-20">
+                                    <p className="font-medium">Offline</p>
+                                </td>
+                                <td className="px-7 2xl:px-0">
+                                    <Link href={`/tutor/${tutor.sid}`}>
+                                        <button className="mx-2 my-2 bg-white transition duration-150 ease-in-out hover:border-gray-700 hover:text-gray-600 rounded border border-gray-600 text-gray-500 px-6 py-2 text-xs">
+                                            View
+                                        </button>
+                                    </Link>
+                                </td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
+            </div>
         </div>
-    )
+    );
 }
