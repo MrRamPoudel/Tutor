5f71c06b5b57b4fbd011da03bf95e2e25e89aa85
Yuan <zy21@uakron.edu>
2023-04-15 17:05:59 -0400

    Update tutor list style

--- pages/tutor/[sid].js
+++ pages/tutor/[sid].js
@@ -56,7 +56,7 @@ const TutorPage = ({ tutor }) => {
                 <ul className="flex flex-col md:flex-row items-start md:items-center text-gray-600 dark:text-gray-600 text-sm mt-3">
                     {tutor.major.map((major) => (
                       <li className="flex items-center mr-4">
-                        <span>{major}</span>
+                        <span>{major} </span>
                       </li>
                     ))}
                 </ul>
@@ -68,11 +68,35 @@ const TutorPage = ({ tutor }) => {
                 <ul className="flex flex-col md:flex-row items-start md:items-center text-gray-600 dark:text-gray-600 text-sm mt-3">
                     {tutor.courses.map((course) => (
                       <li className="flex items-center mr-4">
-                        <span>{course}</span>
+                        <span>{course} </span>
                       </li>
                     ))}
                 </ul>
             </div>
+        </div>
+      </div>
+      <div className="mx-auto container py-20\">
+        <div className="grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
+          <div className="rounded">
+            <div className="w-full h-64 flex flex-col justify-between bg-pink-300 rounded-lg border border-pink-300 mb-6 py-5 px-4">
+              <div>
+                  <h4 className="text-gray-800 font-bold mb-3">Tutor notes</h4>
+                  <p className="text-gray-800 text-sm">Chat me anytime!</p>
+              </div>
+              <div>
+                <div className="flex items-center justify-between text-gray-800">
+                  <p className="text-sm">March 28, 2023</p>
+                  <div className="w-8 h-8 rounded-full bg-gray-800 text-white flex items-center justify-center">
+                      <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-pencil" width={20} height={20} viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round">
+                          <path stroke="none" d="M0 0h24v24H0z" />
+                          <path d="M4 20h4l10.5 -10.5a1.5 1.5 0 0 0 -4 -4l-10.5 10.5v4" />
+                          <line x1="13.5" y1="6.5" x2="17.5" y2="10.5" />
+                      </svg>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </Layout>
