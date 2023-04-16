f1cbfcae0921c8db4e3b541eddbdd64c7a2455d9
Yuan <zy21@uakron.edu>
2023-04-16 03:25:14 -0400

    Remove tutor component

--- components/layout/tutor.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-export default function Tutor(props){
-return (<div >
-<h2 className="pl-1 text-lg font-semibold text-left" >{props.fname} {props.lname}</h2>
-</div>);
-}
