f116a578e96f8408cd8305d63a50b9ae586fb917
Yuan <zy21@uakron.edu>
2023-04-16 03:47:03 -0400

    Add chat room routing and template

---
+++ pages/chat.js
@@ -0,0 +1,12 @@
+import Layout from '@/components/layout/layout'
+import ChatRoom from '@/components/ChatRoom';
+
+const ChatPage = () => {
+  return (
+    <Layout>
+      <ChatRoom />
+    </Layout>
+  )
+}
+
+export default ChatPage;
