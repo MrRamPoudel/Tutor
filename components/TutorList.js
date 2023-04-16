fa3c848efee547e0c0e96eda5e09dd8c50b8c56f
Yuan <zy21@uakron.edu>
2023-04-16 00:26:25 -0400

    Add pagination for tutor list table

--- components/TutorList.js
+++ components/TutorList.js
@@ -3,7 +3,9 @@ import Link from 'next/link';
 
 export default function TutorList() {
     const [tutors, setTutors] = useState([]);
-    const [show, setShow] = useState(null)
+    const [show, setShow] = useState(null);
+    const [currentPage, setCurrentPage] = useState(1);
+    const [itemsPerPage, setItemsPerPage] = useState(10);
 
     useEffect(() => {
         async function fetchTutors() {
@@ -14,9 +16,25 @@ export default function TutorList() {
         }
       
         fetchTutors();
-      }, []);
+    }, []);
 
-      return (
+    function scrollToTop() {
+        window.scrollTo({
+          top: 0,
+          behavior: 'smooth',
+        });
+    }
+
+    function handlePageChange(newPage) {
+        setCurrentPage(newPage);
+        scrollToTop();
+    }
+
+    const indexOfLastItem = currentPage * itemsPerPage;
+    const indexOfFirstItem = indexOfLastItem - itemsPerPage;
+    const currentItems = tutors.slice(indexOfFirstItem, indexOfLastItem);
+
+    return (
         <div className="w-full sm:px-6">
             <div className="bg-white shadow px-4 md:px-10 pt-4 md:pt-7 pb-5 overflow-y-auto">
                 <table className="w-full whitespace-nowrap">
@@ -29,7 +47,7 @@ export default function TutorList() {
                         </tr>
                     </thead>
                     <tbody className="w-full">
-                        {tutors.map((tutor) => (
+                        {currentItems.map((tutor) => (
                             <tr key={tutor.sid} className="h-20 text-sm leading-none text-gray-800 bg-white hover:bg-gray-100 border-b border-t border-gray-100">
                                 <td className="pl-4 cursor-pointer">
                                     <div className="flex items-center">
@@ -63,6 +81,41 @@ export default function TutorList() {
                         ))}
                     </tbody>
                 </table>
+
+                <div className="flex items-center justify-center py-10 lg:px-0 sm:px-6 px-4">
+                    <div className=" w-full  flex items-center justify-between border-t border-gray-200">
+                        <div className="flex items-center pt-3 text-gray-600 hover:text-indigo-700 cursor-pointer">
+                            <svg width={14} height={8} viewBox="0 0 14 8" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <path d="M1.1665 4H12.8332" stroke="currentColor" strokeWidth="1.25" strokeLinecap="round" strokeLinejoin="round" />
+                                <path d="M1.1665 4L4.49984 7.33333" stroke="currentColor" strokeWidth="1.25" strokeLinecap="round" strokeLinejoin="round" />
+                                <path d="M1.1665 4.00002L4.49984 0.666687" stroke="currentColor" strokeWidth="1.25" strokeLinecap="round" strokeLinejoin="round" />
+                            </svg>
+                            <p className="text-sm ml-3 font-medium leading-none ">Previous</p>
+                        </div>
+                        <div className="sm:flex hidden">
+                        {Array.from({ length: Math.ceil(tutors.length / itemsPerPage) }, (_, index) => (
+                            <p 
+                                key={index} 
+                                onClick={() => handlePageChange(index + 1)} 
+                                className={`${ currentPage === index + 1 ? 
+                                    "text-indigo-700 border-indigo-400"
+                                    :"text-sm text-gray-600 hover:text-indigo-700 border-transparent hover:border-indigo-400"
+
+                                } font-medium leading-none cursor-pointer border-t pt-3 mr-4 px-2`}>
+                                {index + 1}
+                            </p>
+                        ))}
+                        </div>
+                        <div className="flex items-center pt-3 text-gray-600 hover:text-indigo-700 cursor-pointer">
+                            <p className="text-sm font-medium leading-none mr-3">Next</p>
+                            <svg width={14} height={8} viewBox="0 0 14 8" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <path d="M1.1665 4H12.8332" stroke="currentColor" strokeWidth="1.25" strokeLinecap="round" strokeLinejoin="round" />
+                                <path d="M9.5 7.33333L12.8333 4" stroke="currentColor" strokeWidth="1.25" strokeLinecap="round" strokeLinejoin="round" />
+                                <path d="M9.5 0.666687L12.8333 4.00002" stroke="currentColor" strokeWidth="1.25" strokeLinecap="round" strokeLinejoin="round" />
+                            </svg>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     );
